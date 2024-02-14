4a625cdabf8cd765947c688d26a2701cee7c3f43
FrydmanPiotr <98648816+FrydmanPiotr@users.noreply.github.com>
2024-02-14 13:56:02 +0100

    Update and rename klient_api.py to client_api.py

--- client_api.py
+++ client_api.py
@@ -1,19 +1,19 @@
 """
-Nazwa: Aplikacja klient API
-Autor: Piotr Frydman
+Project title: API client application
+Author: Piotr Frydman
 """
 import requests
 import json
 import csv
 
-#Wykonanie wywołania API i przetworzenie odpowiedzi.
+#make an API call and process the response
 url = 'https://danepubliczne.imgw.pl/api/data/synop'
 response = requests.get(url)
 if response.status_code != requests.codes.ok:
     print("Błąd połączenia z API")
 
 else:
-    #utworzenie pliku 
+    #create file 
     response_dict = response.json()
     with open("dane_imgw.csv", 'w',newline="") as file:
         csvwriter=csv.writer(file, delimiter=";")
@@ -21,13 +21,13 @@ else:
         csvwriter.writerow(headers)
     file.close()
 
-    #zapisanie danych z odpowiedzi do pliku
+    #saving data from response to file
     with open("dane_imgw.csv", 'a',newline="") as file:
         csvwriter=csv.writer(file, delimiter=";")
         data = []
         for station in response_dict:
             data.append(station['stacja'])
-            #replace - zamiana znaków dla poprawnego formatowania Excel
+            #replace - swapping signs to correct format insert in Excel
             data.append(station['temperatura'].replace(".", ","))
             data.append(station['cisnienie'])
             data.append(station['predkosc_wiatru'])
