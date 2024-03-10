908a68d2184c4209772e86b07570fa241c4e4f3c
Piotr Frydman <piotrfrydman99@gmail.com>
2024-03-10 18:46:12 +0100

    utworzenie bazy danych dla odczytów z Api IMGW

--- imgwApiClient.py
+++ imgwApiClient.py
@@ -1,79 +1,73 @@
 """
-Project title: API client application
+Project title: IGMW API client application
 Author: Piotr Frydman
 """
 import requests
 import json
 import csv
+import sqlite3
 import time
 from plotly.graph_objs import Bar
 from plotly import offline
 
-#launching program from console
-if __name__=="__main__":
-    #make an API call and process the response
-    url = 'https://danepubliczne.imgw.pl/api/data/synop'
-    response = requests.get(url)
-    if response.status_code != requests.codes.ok:
-        print("Błąd połączenia z API")
+# make an API call and process the response
+url = 'https://danepubliczne.imgw.pl/api/data/synop'
+response = requests.get(url)
+if response.status_code != requests.codes.ok:
+    print("Błąd połączenia z API")
+    exit()
 
-    else:
-        response_dict = response.json()
-        
-        #saving data from response to file
-        data = []
-        current_date = time.strftime("%d-%m-%Y", time.localtime())
-        filename = f"dane_imgw_{current_date}.csv"
-        with open(filename, 'w',newline="") as file:
-            csvwriter=csv.writer(file, delimiter=";")
-            headers=['Lokalizacja','Temperatura','Ciśnienie','Wiatr','Opady']
-            csvwriter.writerow(headers)
-        
-            for station in response_dict:
-                data.append(station['stacja'])
-                #replace - swapping signs to correct format insert in Excel
-                data.append(station['temperatura'].replace(".", ","))
-                data.append(station['cisnienie'])
-                data.append(station['predkosc_wiatru'])
-                data.append(station['suma_opadu'].replace(".", ","))
-                csvwriter.writerow(data)
-                data.clear()
-        file.close()
+response_dict = response.json()
 
-        #sort data 
-        raw_dict = {}
-        stations, temperature = [], []
-        for station in response_dict:
-            raw_dict[station['stacja']] = station['temperatura']
-            sort_dict = sorted(raw_dict.items(),key=lambda x: x[1])
-        for elements in sort_dict:
-            stations.append(elements[0])
-            temperature.append(elements[1])
-         
-        #creating visualisation
-        data = [{
-            'type':'bar',
-            'x': stations,
-            'y': temperature,
-            'marker':{
-                'color':'rgb(180,20,20)',
-                'line':{'width':1.5,'color': 'rgb(50,25,25)'}
-                },
-            'opacity':0.6,
-            }]
-        layout={
-            'title': f'Temperatura we wszystkich stacjach IMGW ({current_date})',
-            'titlefont': {'size':28},
-            'xaxis': {
-                'title':'Temperatura',
-                'titlefont':{'size':24},
-                'tickfont':{'size':14},
-                },
-            'yaxis': {
-                'title': 'Lokalizacja stacji',
-                'titlefont':{'size':24},
-                'tickfont':{'size':14},
-                },
-            }
-        fig={'data':data,'layout': layout}
-        offline.plot(fig, filename='temperature_imgw.html')
+# Create a table name without special characters
+current_date = time.strftime("%d_%m_%Y", time.localtime())
+table_name = f"IMGW_{current_date}"
+filename = f"imgw_{current_date}.csv"
+
+conn = sqlite3.connect('imgw.sqlite')
+c = conn.cursor()
+
+# creating table
+c.execute(f'''CREATE TABLE IF NOT EXISTS {table_name}
+        (stacja TEXT,
+        temperatura FLOAT,
+        ciśnienie INTEGER,
+        wiatr INTEGER,
+        opady INTEGER)''')
+
+c.execute(f"delete from {table_name}")
+
+for station in response_dict:
+    stacja = station['stacja']
+    temperatura = station['temperatura']
+    cisnienie = station['cisnienie']
+    wiatr = station['predkosc_wiatru']
+    opady = station['suma_opadu']
+
+    c.execute(f"INSERT INTO {table_name} (stacja, temperatura, ciśnienie,wiatr,opady) VALUES (?, ?, ?,?,?)",
+              (stacja, temperatura, cisnienie,wiatr,opady))
+      
+#saving changes and close connection
+conn.commit()
+conn.close()
+
+conn = sqlite3.connect('imgw.sqlite')
+c = conn.cursor()
+for db in c.execute("SELECT name FROM sqlite_schema WHERE type ='table' AND name NOT LIKE 'sqlite_%';"):
+    print(db)
+
+#saving data from response to file
+with open(filename, 'w',newline="") as file:
+    csvwriter=csv.writer(file, delimiter=";")
+    headers=['Lokalizacja','Temperatura','Ciśnienie','Wiatr','Opady']
+    csvwriter.writerow(headers)
+
+    #display columns names
+    querry = c.execute(f'SELECT * FROM {table_name}')
+    for data in querry:
+        print(data)
+        csvwriter.writerow(data)
+file.close()
+
+
+c.close()
