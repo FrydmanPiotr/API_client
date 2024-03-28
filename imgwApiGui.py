84e3749f7bc2bb16fbeee723c6bf294d028d43b6
Piotr Frydman <piotrfrydman99@gmail.com>
2024-03-28 11:50:37 +0100

    modifying user interface

--- imgwApiGui.py
+++ imgwApiGui.py
@@ -8,38 +8,27 @@ from tkinter import ttk
 
 class imgwApiGui:
     def __init__(self, main):
-        frame = tk.Frame(main)
-        frame.pack()
         main.title("IMGW Api Client")
-        main.geometry("600x300+500+250")
+        main.geometry("500x280+500+250")
         main.resizable(False, False)
-        
-        #grid of elments
-        frame.columnconfigure(0, weight=3)
-        main.columnconfigure(1, weight=1)
-        main.columnconfigure(2, weight=2)
-        main.rowconfigure(0, weight=3)
-        main.rowconfigure(1, weight=1)
-        main.rowconfigure(2, weight=1)
-        self.create_widgets(frame)
+        self.create_widgets(main)
 
-    def create_widgets(self, frame):
+    def create_widgets(self,main):
         #shows records
-        self.table = ttk.Treeview(frame)
-        self.table.grid(row=0,column=0)
+        columns=('City','Temperature', 'Pressure','Wind speed','Rainfall total')
+        table = ttk.Treeview(main,height=8,columns=columns,show='headings')
+        for column in table["columns"]:
+            table.column(column, stretch=False, width=90)
+            table.heading(column,text=column)
+        table.place(x=23,y=15)
         
-        self.date_label = tk.Label(frame, text="Date")
-        self.date_label.grid(row=1, column=0)
-        self.date_combo = ttk.Combobox(frame, state="readonly")
-        self.date_combo.grid(row=1, column=1)
+        tk.Label(main, text="Date").place(x=20,y=220)
+        ttk.Combobox(main, state="readonly").place(x=53,y=220)
         
-        self.hour_label = tk.Label(frame, text="Hour")
-        self.hour_label.grid(row=2, column=0)
-        self.hour_combo = ttk.Combobox(frame, state="readonly")
-        self.hour_combo.grid(row=2, column=1)
-        
-        self.select_button = tk.Button(frame, text="Show", pady=5, padx=5)
-        self.select_button.grid(row=2, column=2)
+        tk.Label(main, text="Hour").place(x=210,y=220)
+        ttk.Combobox(main, state="readonly").place(x=250,y=220)
+        btn=tk.Button(main, text="Show",width=6,height=2,bd=3,bg="#FFE5CC")
+        btn.place(x=410, y=210)
         
 root = tk.Tk()
 imgwApiGui(root)
