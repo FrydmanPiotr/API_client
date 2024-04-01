d6b87a93492cea8c8e8d3ceddff8c531b4c54598
FrydmanPiotr <98648816+FrydmanPiotr@users.noreply.github.com>
2024-04-01 16:36:12 +0200

    Add files via upload
    Graphical user interface merge to rest of project

--- imgwApiClient.py
+++ imgwApiClient.py
@@ -1,133 +1,105 @@
 """
-Project title: API client application
+Project title: IGMW API client application
 Author: Piotr Frydman
 """
 
+import tkinter as tk
+from tkinter import ttk,messagebox
 import requests
-import json
 import csv
 import sqlite3 as db
 import time
-from plotly.graph_objs import Bar
-from plotly import offline
 
 current_date = time.strftime("%d_%m_%Y", time.localtime())
 table_name = f"imgw_{current_date}"
-filename = table_name + ".csv"
 
-def api_request():
-    #make an API call and process the response
+def get_data():
     url = 'https://danepubliczne.imgw.pl/api/data/synop'
     try:
-        response = requests.get(url)
-        response_dict = response.json()
-        save_data_in_db(response_dict)
-    except Exception as e:
-        print("Error:", e)
+        response = requests.get(url).json()
+        conn = db.connect("imgw.sqlite")
+        c = conn.cursor()
 
-def connect_db():
-    db_name = "imgw.sqlite"
-    conn = db.connect(db_name)
-    return conn
-
-def save_data_in_db(response_dict):
-    #connect to database
-    conn = connect_db()
-    c = conn.cursor()
-
-    #creating table
-    querry = f'''CREATE TABLE IF NOT EXISTS {table_name}
-            (stacja TEXT,
-            temperatura FLOAT,
-            ciśnienie INTEGER,
-            wiatr INTEGER,
-            opady INTEGER)'''
-    c.execute(querry)
-
-    #clear existing data 
-    querry = f"DELETE FROM {table_name}"
-    c.execute(querry)
-
-    #inserting data
-    for station in response_dict:
-        stacja = station['stacja']
-        temperatura = station['temperatura']
-        cisnienie = station['cisnienie']
-        wiatr = station['predkosc_wiatru']
-        opady = station['suma_opadu']
-
-        querry = f'''INSERT INTO {table_name} (stacja, temperatura, ciśnienie, wiatr, opady)
-            VALUES (?, ?, ?, ?, ?)'''
-        c.execute(querry, (stacja, temperatura, cisnienie, wiatr, opady))
-
-    conn.commit()
-    conn.close()
-
-def save_data_in_csv():
-    #connect to database
-    conn = connect_db()
-    c = conn.cursor()
-
-    #saving data from database to CSV file
-    with open(filename, 'w', newline="") as file:
-        csvwriter = csv.writer(file, delimiter=";")
-        headers = ['Lokalizacja', 'Temperatura', 'Ciśnienie', 'Wiatr', 'Opady']
-        csvwriter.writerow(headers)
-
-        #writing data from database to CSV
-        querry = f'SELECT * FROM {table_name}'
+        querry = f'''CREATE TABLE IF NOT EXISTS {table_name}
+            (City TEXT,
+            Temperature FLOAT,
+            Pressure INTEGER,
+            Wind_speed INTEGER,
+            Rainfall_total INTEGER)'''
         c.execute(querry)
-        for row in c.fetchall():
-            row = [str(val).replace('.', ',') if isinstance(val, float) else val for val in row]
-            csvwriter.writerow(row)
-    conn.close()
 
-def create_visualisation():
-    #connect to database
-    conn = connect_db()
+        c.execute(f"SELECT * FROM {table_name}")
+        if not c.fetchall():
+            querry = f'''INSERT INTO {table_name} (City, Temperature, Pressure,
+                Wind_speed, Rainfall_total) VALUES (?, ?, ?, ?, ?)'''
+            for station in response:
+                city = station['stacja']
+                temperature = station['temperatura']
+                pressure = station['cisnienie']
+                wind = station['predkosc_wiatru']
+                rainfall = station['suma_opadu']
+                c.execute(querry, (city, temperature, pressure, wind, rainfall))
+            conn.commit()
+    except:
+        tk.messagebox.showerror("Error", "Connection with IMGW API failed")
+
+def display_records(table):
+    conn = db.connect("imgw.sqlite")
     c = conn.cursor()
-
-    #fetching data from the database
-    querry = f'SELECT stacja, temperatura FROM {table_name} ORDER BY temperatura ASC'
-    station, parameter = [], []
-
+    
+    querry = f"SELECT * FROM {table_name} order by City asc"
     c.execute(querry)
-    for stacja, temperatura in c.fetchall():
-        station.append(stacja)
-        parameter.append(temperatura)
-
-    #creating visualization
-    data = [{
-        'type': 'bar',
-        'x': station,
-        'y': parameter,
-        'marker': {
-            'color': 'rgb(180,20,20)',
-            'line': {'width': 1.5, 'color': 'rgb(50,25,25)'}
-        },
-        'opacity': 0.6,
-    }]
-    layout = {
-        'title': f'Temperatura we wszystkich stacjach IMGW ({current_date})',
-        'titlefont': {'size': 28},
-        'xaxis': {
-            'title': 'Temperatura (*C)',
-            'titlefont': {'size': 24},
-            'tickfont': {'size': 14},
-        },
-        'yaxis': {
-            'title': 'Lokalizacja stacji',
-            'titlefont': {'size': 24},
-            'tickfont': {'size': 14},
-        },
-    }
-    fig = {'data': data, 'layout': layout}
-    offline.plot(fig, filename='temperature_imgw.html')
+    for row in c.fetchall():
+        table.insert("", tk.END, values=row)
+    c.close()
     conn.close()
 
+def save_in_csv():
+    conn = db.connect("imgw.sqlite")
+    c = conn.cursor()
+    filename = table_name + ".csv"
+    columns = ('City', 'Temperature', 'Pressure', 'Wind_speed', 'Rainfall_total')
+    try:
+        c.execute(f'SELECT * FROM {table_name} order by City asc')
+        with open(filename, 'w', newline="") as file:
+            csvwriter = csv.writer(file, delimiter=";")
+            csvwriter.writerow(columns)
+        
+            for row in c.fetchall():
+                row = [str(val).replace('.', ',') if isinstance(val, float)
+                       else val for val in row]
+                csvwriter.writerow(row)
+            tk.messagebox.showinfo("Saving in CSV", "File was created")
+    except Exception as e :
+        tk.messagebox.showerror("Error", f"{e}")
+        
+def create_main_window():
+    root.title(f"IMGW Api Client  |  {current_date}")
+    root.geometry("500x310+500+250")
+    root.resizable(False, False)
+    
+    columns = ('City', 'Temperature', 'Pressure', 'Wind_speed',
+               'Rainfall_total')
+    table = ttk.Treeview(root, height=8, columns=columns, show='headings')
+    for column in columns:
+        table.column(column, stretch=False, width=90)
+        table.heading(column, text=column)
+    table.place(x=23, y=15)
+    
+    export = tk.Button(root, text="Export", width=6, bd=3, bg="#FFE5CC",
+                       command=save_in_csv)
+    export.place(x=180, y=210)
+    #connect with api
+    get_data()
+    display_records(table)
+        
 try:
-    api_request()
-    save_data_in_csv()
-    create_visualisation()
+    root = tk.Tk()
+    create_main_window()
 except Exception as e:
-    print("Error:", e)
+    tk.messagebox.showerror("Error", f"{e}")
+
+root.mainloop()
+
+    
+
