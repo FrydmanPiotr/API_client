6867de3be1ba74117df4ebc999929f0f3c17e782
FrydmanPiotr <98648816+FrydmanPiotr@users.noreply.github.com>
2024-04-10 19:14:43 +0200

    Refactoring code

--- imgwApiClient.py
+++ imgwApiClient.py
@@ -10,34 +10,34 @@ import requests
 import time
 import csv
 
-class imgwApiClient(tk.Tk):
+class ImgwApiClient(tk.Tk):
     def __init__(self):
         super().__init__()
-        current_date = time.strftime("%d_%m_%Y", time.localtime())
-        self.title(f"IMGW Api Client  |  Date: {current_date}")
+        self.date=time.strftime('%d_%m_%Y', time.localtime())
+        self.table_name = f"imgw_{self.date}"
+        self.conn = db.connect("imgw.sqlite")
+        self.cur = self.conn.cursor()
+        self.setup_ui()
+        self.load_menu()      
+
+    def setup_ui(self):
+        self.title(f"IMGW Api Client  |  Date: {self.date}")
         self.geometry("500x310+500+250")
         self.resizable(False, False)
-        self.table_name = f"imgw_{current_date}"
-        self.create_main_window()
-
-    def get_data(self):
-        url = 'https://danepubliczne.imgw.pl/api/data/synop'
+    
+    def get_data(self,table): 
         try:
-            respons = requests.get(url).json()
-            conn = db.connect("imgw.sqlite")
-            c = conn.cursor()
-
-            querry = f'''CREATE TABLE IF NOT EXISTS {self.table_name}
-                (City TEXT,
-                Temperature FLOAT,
-                Pressure INTEGER,
-                Wind_speed INTEGER,
+            url = 'https://danepubliczne.imgw.pl/api/data/synop'
+            response = requests.get(url).json()            
+            query = f'''CREATE TABLE IF NOT EXISTS {self.table_name}
+                (City TEXT, Temperature FLOAT,
+                Pressure INTEGER, Wind_speed INTEGER,
                 Rainfall_total INTEGER)'''
-            c.execute(querry)
+            self.cur.execute(query)
 
-            c.execute(f"SELECT * FROM {self.table_name}")
-            if not c.fetchall():
-                querry = f'''INSERT INTO {self.table_name}
+            self.cur.execute(f"SELECT * FROM {self.table_name}")
+            if not self.cur.fetchall():
+                query = f'''INSERT INTO {self.table_name}
                     (City, Temperature, Pressure,
                     Wind_speed, Rainfall_total) VALUES (?, ?, ?, ?, ?)'''
                 for station in response:
@@ -46,35 +46,32 @@ class imgwApiClient(tk.Tk):
                     pressure = station['cisnienie']
                     wind = station['predkosc_wiatru']
                     rainfall = station['suma_opadu']
-                    c.execute(querry, (city, temperature, pressure, wind, rainfall))
-                conn.commit()
-                
-        except:
-            tk.messagebox.showerror("Error", "Connection with IMGW API failed")
+                    self.cur.execute(query, (city, temperature, pressure, wind,
+                                      rainfall))
+                self.conn.commit()
+            self.display_data(table)
+        except Exception as e:
+            tk.messagebox.showerror("Error", f"{e}")
 
-    def display_records(self,table):
-        conn = db.connect("imgw.sqlite")
-        c = conn.cursor()
-        
-        querry = f"SELECT * FROM {self.table_name} order by City asc"
-        c.execute(querry)
-        for row in c.fetchall():
+    def display_data(self,table):
+        query = f"SELECT * FROM {self.table_name} order by City asc"
+        self.cur.execute(query)
+            
+        for row in self.cur.fetchall():
             table.insert("", tk.END, values=row)
-        c.close()
-        conn.close()
+        self.cur.close()
+        self.conn.close()
 
-    def save_in_csv(self):
-        conn = db.connect("imgw.sqlite")
-        c = conn.cursor()
-        filename = self.table_name + ".csv"
-        columns = ('City', 'Temperature', 'Pressure', 'Wind_speed', 'Rainfall_total')
+    def export_csv(self,columns):
+        file = f'{self.table_name}.csv'
         try:
-            c.execute(f'SELECT * FROM {self.table_name} order by City asc')
-            with open(filename, 'w', newline="") as file:
-                csvwriter = csv.writer(file, delimiter=";")
+            query = f'SELECT * FROM {self.table_name} order by City asc'
+            self.cur.execute(query)
+            with open(file, 'w', newline="") as f:
+                csvwriter = csv.writer(f, delimiter=";")
                 csvwriter.writerow(columns)
             
-                for row in c.fetchall():
+                for row in self.cur.fetchall():
                     row = [str(val).replace('.', ',') if isinstance(val, float)
                            else val for val in row]
                     csvwriter.writerow(row)
@@ -82,7 +79,7 @@ class imgwApiClient(tk.Tk):
         except Exception as e :
             tk.messagebox.showerror("Error", f"{e}")
             
-    def create_main_window(self):    
+    def load_menu(self):
         columns = ('City', 'Temperature', 'Pressure', 'Wind_speed',
                    'Rainfall_total')
         table = ttk.Treeview(self, height=8, columns=columns, show='headings')
@@ -91,17 +88,10 @@ class imgwApiClient(tk.Tk):
             table.heading(column, text=column)
         table.place(x=23, y=15)
         
-        export = tk.Button(self, text="Export", width=6, bd=3, bg="#FFE5CC",
-                           command=self.save_in_csv)
-        export.place(x=180, y=210)
-        update = tk.Button(self, text="Update", width=6, bd=3, bg="#FFE5CC",
-                           command=self.get_data)
-        update.place(x=250, y=210)
-        #show records
-        self.display_records(table)
+        tk.Button(self, text="Export", width=6, bd=3, bg="#FFE5CC",
+                command=lambda: self.export_csv(columns)).place(x=180, y=210)
+        tk.Button(self, text="Load", width=6, bd=3, bg="#FFE5CC",
+                  command=lambda: self.get_data(table)).place(x=250, y=210)
             
-try:
-    iac = imgwApiClient()
-    iac.mainloop()
-except Exception as e:
-    tk.messagebox.showerror("Error", f"{e}") 
+iac = ImgwApiClient()
+iac.mainloop()    
